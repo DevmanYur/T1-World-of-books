6d3a2a3b1eef9ae30bed790c367efdcb7dcb2994
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-08-04 10:56:59 +0300

    up

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from my_app import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    re_path(r'^books/$', views.index, name='books2'),
+    re_path(r'^avts/$', views.index, name='avts2'),
+    # url(r'^books/$', views.BookListView.as_view(), name='books'),
 ]
